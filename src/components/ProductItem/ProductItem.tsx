94a191c70a234b155e35c3ac3ee32a39eb2b9f8a
CrisSoria <crissoria07@gmail.com>
2021-09-02 21:17:13 -0300

    products screens

---
+++ src/components/ProductItem/ProductItem.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import {Image, Text, View} from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import styles from './styles';
+
+interface ProductItem {
+  item: {
+    id: string;
+    title: string;
+    image: string;
+    avgRating: number;
+    ratings: number;
+    price: number;
+    oldPrice?: number;
+  };
+}
+
+const ProductItem = ({item}: ProductItem) => {
+  function printRatingStars(rating: number) {
+    const starFull = Math.trunc(rating);
+    const startHalf = rating - starFull > 0;
+    const emptyStars = 5 - starFull;
+
+    const componente = [];
+
+    const compStar = (name: string, indx: number) => (
+      <FontAwesome
+        key={indx}
+        style={styles.star}
+        name={name}
+        size={18}
+        color={'#e47911'}
+      />
+    );
+
+    for (let i = 0; i < starFull; i++) {
+      componente.push(compStar('star', i));
+    }
+    if (startHalf) {
+      componente.push(compStar('star-half-full', 6));
+    }
+    for (let i = 1; i < emptyStars; i++) {
+      componente.push(compStar('star-o', i + 7));
+    }
+
+    return componente;
+  }
+
+  return (
+    <View style={styles.root}>
+      <Image
+        style={styles.image}
+        source={{
+          uri: item.image,
+        }}
+      />
+      <View style={styles.rightContainer}>
+        <Text style={styles.title} numberOfLines={3}>
+          {item.title}
+        </Text>
+        {/* Ratings */}
+        <View style={styles.ratingsContainer}>
+          {printRatingStars(item.avgRating)}
+          <Text>{item.ratings}</Text>
+        </View>
+        {/* Price */}
+        <Text style={styles.price}>
+          $ {item.price}
+          {item.oldPrice && (
+            <Text style={styles.oldPrice}> ${item.oldPrice}</Text>
+          )}
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+export default ProductItem;
